98ce30f6652d3586e3c5751c7c7bd95eba9600a4
Puddle <Puddleglumstudio@gmail.com>
2024-06-08 18:38:37 -0400

    Dynarray and Vector labs update

--- DSA Labs/DSA Labs/Lab2.h
+++ DSA Labs/DSA Labs/Lab2.h
@@ -31,19 +31,19 @@ NOTE: If the unit test is not on, that code will not be compiled!
 */
 
 // Main toggle
-#define LAB_2	0
+#define LAB_2	1
 
 // Individual unit test toggles
-#define LAB2_PALINDROME_NUMBER		0
-#define LAB2_FILL_FILE				0
-#define LAB2_FILL_ARRAY				0
-#define LAB2_CLEAR					0
-#define LAB2_SORT_ASCENDING			0
-#define LAB2_SORT_DESCENDING		0
-#define LAB2_BRACKETS				0
-#define LAB2_CONTAINS_TRUE			0
-#define LAB2_CONTAINS_FALSE			0
-#define LAB2_MOVE_PALINDROMES		0
+#define LAB2_PALINDROME_NUMBER		1
+#define LAB2_FILL_FILE				1
+#define LAB2_FILL_ARRAY				1
+#define LAB2_CLEAR					1
+#define LAB2_SORT_ASCENDING			1
+#define LAB2_SORT_DESCENDING		1
+#define LAB2_BRACKETS				1
+#define LAB2_CONTAINS_TRUE			1
+#define LAB2_CONTAINS_FALSE			1
+#define LAB2_MOVE_PALINDROMES		1
 
 /************/
 /* Includes */
@@ -56,7 +56,10 @@ NOTE: If the unit test is not on, that code will not be compiled!
 
 inline int IsPalindromeNumber(unsigned int _num) {
 	// TODO: Implement this function	
-	
+	std::string reverseNum = std::to_string(_num);
+	std::string wholeNum = reverseNum;
+	std::reverse(reverseNum.begin(), reverseNum.end());
+	return wholeNum == reverseNum ? true : false;
 }
 
 class DSA_Lab2
@@ -72,36 +75,65 @@ public:
 
 	void Fill(const char* _inputFilename) {
 		// TODO: Implement this method
-
+		
 	}
 
 	void Fill(const int* _arr, size_t _size) {
 		// TODO: Implement this method
-
+		mValues.clear();
+		for (size_t i = 0; i < _size; i++)
+		{
+			mValues.push_back(_arr[i]);
+		}
 	}
 
 	void Clear() {
 		// TODO: Implement this method
-
+		mValues.clear();
+		std::vector<int>().swap(mValues);
 	}
-
 	void Sort(bool _ascending) {
+
 		// TODO: Implement this method
+		if (_ascending) {
+			std::sort(mValues.begin(), mValues.end());
+		}
+		else {
+			std::sort(mValues.begin(), mValues.end(), std::greater<>{});
+		}
 
 	}
 
 	int operator[](int _index) {
 		// TODO: Implement this method
-	
+		return mValues[_index];
 	}
 
 	bool Contains(int _val) const {
 		// TODO: Implement this method
-	
+		for (size_t i = 0; i < mValues.size(); i++)
+		{
+			if (mValues[i] == _val) return true;
+		}
+		return false;
 	}
 
 	void MovePalindromes() {	
 		// TODO: Implement this method
+		std::vector<int> tempIndex;
+		for (size_t i = 0; i < mValues.size(); i++)
+		{
+			if (IsPalindromeNumber(mValues[i])) {
+
+				mPalindromes.push_back(mValues[i]);
+
+			}
+			else {
+
+				tempIndex.push_back(mValues[i]);
+			}
+		}
+		mValues = tempIndex;
 
 	}
 };
